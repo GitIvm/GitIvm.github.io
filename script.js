0faee6417b065eef6c56ebad275919f979f25149
GitIvm <163049868+GitIvm@users.noreply.github.com>
2024-04-26 17:21:44 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-document.getElementById('toggleTextButton').addEventListener('click', function() {
-    var lyricsDiv = document.getElementById('lyrics');
-    if (lyricsDiv.style.display === 'none') {
-        lyricsDiv.style.display = 'block';
-        this.textContent = 'Сховати текст';
+function toggleLyrics() {
+    var lyricsContainer = document.getElementById("lyrics");
+    var lyricsButton = document.querySelector(".buttons button:first-child");
+    if (lyricsContainer.style.display === "none" || lyricsContainer.style.display === "") {
+        lyricsContainer.style.display = "block";
+        lyricsButton.textContent = "Сховати текст";
     } else {
-        lyricsDiv.style.display = 'none';
-        this.textContent = 'Показати текст';
+        lyricsContainer.style.display = "none";
+        lyricsButton.textContent = "Показати текст";
     }
-});
+}
 
-document.getElementById('downloadButton').addEventListener('click', function() {
-    var audioSrc = document.querySelector('audio').src;
+
+function downloadSong() {
     var link = document.createElement('a');
-    link.href = audioSrc;
+    link.href = 'Skryabin - Випускний.mp3';
     link.download = 'Skryabin - Випускний.mp3';
+    document.body.appendChild(link);
     link.click();
-});
+    document.body.removeChild(link);
+}
