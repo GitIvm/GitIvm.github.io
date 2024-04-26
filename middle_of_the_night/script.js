8426e137ec78892057b64801b9e00f0eaff36157
GitIvm <163049868+GitIvm@users.noreply.github.com>
2024-04-26 18:24:13 +0300

    Update script.js

--- middle_of_the_night/script.js
+++ middle_of_the_night/script.js
@@ -3,8 +3,8 @@
 
 function downloadSong() {
     var link = document.createElement('a');
-    link.href = 'Skryabin - Випускний.mp3';
-    link.download = 'Skryabin - Випускний.mp3';
+    link.href = 'Middle Of The Night.mp3';
+    link.download = 'Middle Of The Night.mp3';
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
