f1dbeb963122cf62acd7a5efa632840b37b0874c
GitIvm <163049868+GitIvm@users.noreply.github.com>
2024-04-26 18:18:41 +0300

    Add files via upload

---
+++ middle_of_the_night/script.js
@@ -0,0 +1,11 @@
+
+
+
+function downloadSong() {
+    var link = document.createElement('a');
+    link.href = 'Skryabin - Випускний.mp3';
+    link.download = 'Skryabin - Випускний.mp3';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
